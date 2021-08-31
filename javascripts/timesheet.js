b773153a5e446b0bd7b24a24d6479983a7f353da
Steven Choi <sjc@ualberta.ca>
2021-08-30 22:18:42 -0600

    Validation for total cost

--- javascripts/timesheet.js
+++ javascripts/timesheet.js
@@ -11,9 +11,16 @@ window.onload = function () {
 
     // If any required part of the form is not filled out
     if (!document.querySelector('form').checkValidity()) {
-      console.log('Fill out all required fields.');
+      alert('Fill out all required fields.');
       return;
     }
+
+    document
+      .querySelectorAll('input[name="data[][totalcost]"]')
+      .forEach((node) => {
+        if (node.value === 'NaN') alert('Costs must be a valid number');
+        return;
+      });
 
     // Get form data
     const formData = new FormData(document.querySelector('form'));
@@ -58,26 +65,24 @@ function addRow() {
   // Append new table row
   document.querySelector('tbody').insertAdjacentHTML('beforeend', newTableRow);
 
- // Event handler: quantity * rate = total cost
+  // Event handler: quantity * rate = total cost
   document.querySelectorAll('.num-input').forEach((node) => {
     node.addEventListener('input', (e) => {
       // <tr>
       const rowNodes = e.currentTarget.parentNode.parentNode.children;
-      
-      console.log(rowNodes.item(3).firstElementChild);
+
       // Total Cost <input>
-      rowNodes.item(5).firstElementChild.value = 
-      // Quantity <input>
-      rowNodes.item(3).firstElementChild.value * 
-      // Rate <input>
-      rowNodes.item(4).firstElementChild.value
+      rowNodes.item(5).firstElementChild.value =
+        // Quantity <input>
+        rowNodes.item(3).firstElementChild.value *
+        // Rate <input>
+        rowNodes.item(4).firstElementChild.value;
     });
   });
 
   // Event handler: delete row
   document.querySelectorAll('.delete-btn').forEach((node) =>
     node.addEventListener('click', (e) => {
-      console.log(counter++);
       // remove() the tr parent node
       e.currentTarget.parentNode.remove();
     })
@@ -85,4 +90,4 @@ function addRow() {
 }
 
 const newTableRow =
-  '<tr> <td> <input type="text" name="data[][workorder]" required/> </td><td> <input type="text" name="data[][description]" required/> </td><td> <input type="text" name="data[][unit]" required/> </td><td> <input type="text" class="num-input" name="data[][quantity]" required/> </td><td> <input type="text" class="num-input" name="data[][rate]" required/> </td><td> <input type="text" name="data[][totalcost]" required readonly/> </td><td class="delete-btn"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" > <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg> </td></tr>';
+  '<tr> <td> <input type="text" name="data[][workorder]" required/> </td><td> <input type="text" name="data[][description]" required/> </td><td><input type="text" name="data[][unit]" required/></td><td> <input type="text" class="num-input" name="data[][quantity]" required/> </td><td> <input type="text" class="num-input" name="data[][rate]" required/> </td><td> <input type="text" name="data[][totalcost]" required readonly/> </td><td class="delete-btn"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" > <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg> </td></tr>';
