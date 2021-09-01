983cc6f8504b276557a5e094edda338943e7b8d4
Steven Choi <sjc@ualberta.ca>
2021-08-31 18:32:36 -0600

    LEM Item upload working

--- javascripts/timesheet.js
+++ javascripts/timesheet.js
@@ -15,15 +15,42 @@ window.onload = function () {
       return;
     }
 
+    let costValidFlag = true;
     document
       .querySelectorAll('input[name="data[][totalcost]"]')
       .forEach((node) => {
-        if (node.value === 'NaN') alert('Costs must be a valid number');
-        return;
+        if (costValidFlag && node.value === 'NaN') {
+          alert('Costs must be a valid number');
+          costValidFlag = false;
+        }
       });
 
-    // Get form data
+    if (!costValidFlag) return;
+
+    if (document.querySelector('tbody').childElementCount === 0) {
+      alert('Must have at least one row in the LEM.');
+      return;
+    }
+
+    // Get form data of LEM
     const formData = new FormData(document.querySelector('form'));
+    
+    // Get form table data for LEM Items
+    const tableData = [];
+    // Iterate through table rows and collect data from inputs
+    for (let i = 0; i < document.querySelector('tbody').childElementCount; ++i) {      
+      const cells = document.querySelector('tbody').children.item(i).children;
+      const rowData = {
+        workOrder: cells.item(0).firstElementChild.value.trim(),
+        description: cells.item(1).firstElementChild.value.trim(),
+        unit: cells.item(2).firstElementChild.value.trim(),
+        quantity: cells.item(3).firstElementChild.value.trim(),
+        rate: cells.item(4).firstElementChild.value.trim(),
+        totalCost: cells.item(5).firstElementChild.value.trim(),
+      };
+      tableData.push(rowData);
+    }
+
     const formDataObj = {
       client: formData.get('client'),
       location: formData.get('location'),
@@ -36,22 +63,18 @@ window.onload = function () {
     // Add LEM and all related items
     db.transaction('rw', db.LEMs, db.LEMItems, async () => {
       const insertedID = await db.LEMs.add(formDataObj);
-      if (document.querySelector('tbody').childElementCount === 1) {
+
+      for (const obj of tableData) {
+        console.log(obj);
         db.LEMItems.add({
           LEMid: insertedID,
-          workOrder: formData.get('data[][workorder]'),
-          description: formData.get('data[][description]'),
-          quantity: formData.get('data[][quantity]'),
-          rate: formData.get('data[][rate]'),
-          totalCost: formData.get('data[][totalcost]'),
-        });
-      } else if (document.querySelector('tbody').childElementCount > 1) {
-        const dataArr = formData.get('data');
-        for (const obj of dataArr) {
-          console.log(obj);
-        }
-      } else {
-        console.log('Error determining rowcount of data entered.');
+          workOrder: obj.workOrder.trim(),
+          description: obj.description.trim(),
+          unit: obj.unit.trim(),
+          quantity: obj.quantity.trim(),
+          rate: obj.rate.trim(),
+          totalCost: obj.totalCost
+        })
       }
     }).catch((err) => {
       console.error(err);
@@ -90,4 +113,4 @@ function addRow() {
 }
 
 const newTableRow =
-  '<tr> <td> <input type="text" name="data[][workorder]" required/> </td><td> <input type="text" name="data[][description]" required/> </td><td><input type="text" name="data[][unit]" required/></td><td> <input type="text" class="num-input" name="data[][quantity]" required/> </td><td> <input type="text" class="num-input" name="data[][rate]" required/> </td><td> <input type="text" name="data[][totalcost]" required readonly/> </td><td class="delete-btn"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" > <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg> </td></tr>';
+'<tr> <td> <input type="text" required/> </td><td> <input type="text" required/> </td><td><input type="text" required/></td><td> <input type="text" class="num-input" required/> </td><td> <input type="text" class="num-input" required/> </td><td> <input type="text" required readonly/> </td><td class="delete-btn"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" > <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg> </td></tr>';
